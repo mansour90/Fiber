b22a7fd2cc4b0bf4723cba83810a4411260c5a20
mansour90 <mansour_alaa82@yahoo.ro>
2023-06-29 19:09:22 +0300

    Header Section

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
 const openMenu = document.querySelector('.menu-open');
 const closeMenu = document.querySelector('.close-menu');
 const menu = document.querySelector('.mobile-menu');
-const listMenu = document.querySelectorAll('.link-menu')
+const nav = document.querySelector('nav');
+const listMenu = document.querySelectorAll('.link-menu');
 
 openMenu.addEventListener('click', openTheMenu);
 closeMenu.addEventListener('click', closeTheMenu);
@@ -11,9 +12,14 @@ function openTheMenu() {
 }
 
 function closeTheMenu() {
-  menu.classList.remove('open')
+  menu.classList.remove('open');
 }
 
-listMenu.forEach(function(list) {
-  menu.classList.remove('open')
-})
+listMenu.forEach(function (list) {
+  menu.classList.remove('open');
+});
+
+nav.addEventListener('scroll', function (e) {
+  console.log(e.currentTarget.scrollHeight);
+});
+
